22bd927ccd68413eadcaecd1752e2c3c613a7999
pengpn <312924860@qq.com>
2017-12-27 08:53:11 +0800

    upload some unfinished upload product pic

---
+++ app/Models/ProductPricture.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pengpn
+ * Date: 2017/12/26
+ * Time: 下午9:24
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductPricture extends Model
+{
+    protected $table = 'ss_product_pictures';
+    protected $primaryKey = 'photoID';
+}
